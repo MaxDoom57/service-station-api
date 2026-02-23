17030bd8f1efca1ae1b57c9989e12f9207269799
Sandaruwan Rathnayaka <153787764+MaxDoom57@users.noreply.github.com>
2026-02-23 16:28:43 +0530

    remove unwanted files

--- Application/DTOs/VehicleType/VehicleTypeDto.cs
+++
@@ -1,9 +0,0 @@
-namespace Application.DTOs.VehicleType
-{
-    public class VehicleTypeDto
-    {
-        public int CdKy { get; set; }
-        public string Code { get; set; }
-        public string CdNm { get; set; }
-    }
-}
