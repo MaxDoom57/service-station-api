2a42fb80c59b01cf9e106e3a34ddeba2616df4bc
Sandaruwan Rathnayaka <153787764+MaxDoom57@users.noreply.github.com>
2026-02-27 14:32:45 +0530

    delete unwanted files

--- Application/DTOs/Agent/JobResultDto.cs
+++
@@ -1,9 +0,0 @@
-namespace Application.DTOs.Agent
-{
-    public class JobResultDto
-    {
-        public bool Success { get; set; }
-        public string? ResultJson { get; set; }
-        public string? ErrorMessage { get; set; }
-    }
-}
